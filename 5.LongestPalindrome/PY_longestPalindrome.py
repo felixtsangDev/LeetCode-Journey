702e0215cc199127087c3ab3eebc457cecbf5ec8
Chuen <37447380+chuenjai518@users.noreply.github.com>
2021-05-27 11:13:09 +0800

    Update PY_longestPalindrome.py

--- 5.LongestPalindrome/PY_longestPalindrome.py
+++ 5.LongestPalindrome/PY_longestPalindrome.py
@@ -2,9 +2,10 @@ def longestPalindrome(s: str) -> str:
     longest = ""
     for i in range(len(s)):
         temp1 = findPalindrome(s, i, i)
-        temp2 = findPalindrome(s, i, i+1)
+        temp2 = findPalindrome(s, i, i + 1)
         longest = max([longest, temp1, temp2], key=lambda x: len(x))
         # print(f"longest {longest}")
+
     return longest
 
 
@@ -15,8 +16,7 @@ def findPalindrome(s: str, left: int, right: int) -> str:
         right += 1
 
     # print(f"result: {s[left+1:right]}")
-
-    return s[left+1:right]
+    return s[left + 1 : right]
 
 
 print(f"longestPalindrome: {longestPalindrome('abcdefgfedcba')}")
