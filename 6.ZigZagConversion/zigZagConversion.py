cb195c3155a3bec8d5fb5db47ccad3f3c0ca945d
Chuen <37447380+chuenjai518@users.noreply.github.com>
2021-05-28 16:20:12 +0800

    Update zigZagConversion.py

--- 6.ZigZagConversion/zigZagConversion.py
+++ 6.ZigZagConversion/zigZagConversion.py
@@ -2,7 +2,7 @@ def convert(s: str, numRows: int) -> str:
     if numRows == 1 or len(s) < numRows:
         return s
     row_num = 1
-    step = 1
+    step = 1  # decide go down(1) or up(-1)
     rows = {}
     for char in s:
         if row_num not in rows:
@@ -10,6 +10,8 @@ def convert(s: str, numRows: int) -> str:
         else:
             rows[row_num] += char
         row_num += step
+
+        # Change direction when hit top or bottom
         if row_num == 1 or row_num == numRows:
             step *= -1
         # print(rows)
