b01052a6c8bac3457ca97abe6a644a81661d1ddf
Chuen <37447380+chuenjai518@users.noreply.github.com>
2021-05-29 01:01:46 +0800

    Update JS_LongestSubstring_Carmen.js

--- 3.LongestSubstringWithoutRepeatingCharacters/JS_LongestSubstring_Carmen.js
+++ 3.LongestSubstringWithoutRepeatingCharacters/JS_LongestSubstring_Carmen.js
@@ -1,21 +1,20 @@
 var lengthOfLongestSubstring = function (s) {
-  let str = "";
-  let maxCount = 0;
+	let str = "";
+	let maxCount = 0;
 
-  Array.from(s).forEach((c, i) => {
-    if (str.indexOf(c) !== -1) {
-      str = str.slice(str.indexOf(c) + 1);
-    }
-    str += c;
+	Array.from(s).forEach((c, i) => {
+		let tempIndex = str.indexOf(c);
+		if (tempIndex !== -1) {
+			str = str.slice(tempIndex + 1);
+		}
+		str += c;
 
-    if (str.length > maxCount) {
-      maxCount = str.length;
-    }
-  });
+		if (str.length > maxCount) {
+			maxCount = str.length;
+		}
+	});
 
-  return maxCount;
+	return maxCount;
 };
 
-console.log(
-  `lengthOfLongestSubstring: ${lengthOfLongestSubstring("abcabcbb")}`
-);
+console.log(`lengthOfLongestSubstring: ${lengthOfLongestSubstring("abcabcbb")}`);
